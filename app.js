91cad783f5ec24a23778166beb0766ca51d6db5e
elliotsnitzer <50580464+elliotsnitzer@users.noreply.github.com>
2022-04-12 13:54:43 -0400

    attached index.html to server window

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var http = require("http");
+var fs = require("fs");
+
+const hostname = '127.0.0.1';
+const port = 3000;
 
 /* Create an HTTP server to handle responses */
+/* Use a File System to render the html page (index.html) in the server window */
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+fs.readFile('./index.html', function(err, html) {
+  if(err) {
+    throw err;
+  }
+  http.createServer(function(request,response) {
+    response.writeHeader(200, {"Content-Type": "text/html"});  
+    response.write(html);  
+    response.end();
+  }).listen(port, () => {
+    console.log(`Server running at localhost:${port}`);
+  });
+});
