755233a7176b03f31846518500cb1ec5a72c71cc
fridizkool <fsorgtaylor@gmail.com>
2022-04-17 19:19:55 -0400

    added secret

--- spotify_authorization.js
+++ spotify_authorization.js
@@ -3,6 +3,7 @@ const SpotifyWebApi = require("spotify-web-api-node");
 var scopes = ['user-read-private', 'user-read-email'],
   redirectUri = 'http://localhost:3000/home/logged',
   clientId = '7282a035744b40b9a890e04cf8a1fb9e',
+  clientSecret = '25710fcec0c540c3be3c8bf79b99a606',
   state = 'some-state-of-my-choice';
 
 // Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
