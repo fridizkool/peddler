6c84ac5f03955c0908539682188dfa35e2437cf4
Reed Salus <reedsalus@gmail.com>
2022-04-17 20:38:02 -0400

    Revamped spotify auth to include new scopes and use .env

--- spotify_authorization.js
+++ spotify_authorization.js
@@ -1,19 +1,29 @@
+require("dotenv").config();
 const SpotifyWebApi = require("spotify-web-api-node");
 
-var scopes = ['user-read-private', 'user-read-email'],
-  redirectUri = 'http://localhost:3000/home/logged',
-  clientId = '7282a035744b40b9a890e04cf8a1fb9e',
-  clientSecret = '25710fcec0c540c3be3c8bf79b99a606',
-  state = 'some-state-of-my-choice';
+const dataMap = {
+    clientId:           process.env.CLIENT_ID,
+    clientSecret:       process.env.CLIENT_SECRET,
+    redirectUri:        process.env.REDIRECTURI,
+    clientRedirectUri:  process.env.CLIENT_REDIRECTURI,
+    state:              'default',
+    scopes:             ['ugc-image-upload', 'user-modify-playback-state', 'user-read-playback-state',
+                         'user-read-currently-playing', 'user-follow-modify', 'user-follow-read',
+                         'user-read-recently-played', 'user-read-playback-position', 'user-top-read',
+                         'playlist-read-collaborative', 'playlist-modify-public', 'playlist-read-private',
+                         'playlist-modify-private', 'app-remote-control', 'streaming', 'user-read-email',
+                         'user-read-private', 'user-library-modify', 'user-library-read']
+};
 
 // Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
-var spotifyApi = new SpotifyWebApi({
-  redirectUri: redirectUri,
-  clientId: clientId
+let spotifyApi = new SpotifyWebApi({
+    clientId:       dataMap.clientId,
+    clientSecret:   dataMap.clientSecret,
+    redirectUri:    dataMap.redirectUri
 });
 
 // Create the authorization URL
-const authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+const authorizeURL = spotifyApi.createAuthorizeURL(dataMap.scopes, dataMap.state);
 
 exports.authorizeURL = authorizeURL;
 exports.spotifyApi = spotifyApi;
