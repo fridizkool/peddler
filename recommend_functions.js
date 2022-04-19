0dadb87c59afcd2b67ccd3d1e335d24c273cbee7
elliotsnitzer <50580464+elliotsnitzer@users.noreply.github.com>
2022-04-18 20:05:37 -0400

    added preliminary song recommendation code:

--- recommend_functions.js
+++ recommend_functions.js
@@ -2,12 +2,57 @@ const express = require("express");
 const { spotifyApi } = require("./spotify_authorization");
 
 async function songRec(artist, songs) {
-    console.log(songs);
-    console.log(artist);
+    findArtist(artist, function(response){
+        var artistId = response.id;
+        similarArtists(artistId, function(artistList) {
+            preliminaryList(artistList, function(songList) {
+                console.log(songList);
+            });
+        });
+    });
 };
 
-async function similarSongs(song) {
-
+async function preliminaryList(similarArtists, callback) {
+    var songList = [];
+    similarArtists.forEach(function(item) {
+        var currentGenres = item.genres;
+        spotifyApi.getArtistTopTracks(item.id, 'GB')
+        .then(function(data) {
+            data.body.tracks.forEach(function(item) {
+                var song = {id: item.id, name: item.name, genres: currentGenres, popularity: item.popularity};
+                songList.push(song);
+            });
+        }, function(err) {
+            console.log('Something went wrong!', err);
+        });
+    });
+    return callback(songList);
 };
+
+async function calcWeight(songList) {
+    console.log(songList);
+};
+
+async function findArtist(artist, callback) {
+    spotifyApi.searchArtists(artist)
+    .then(function(data) {
+        data.body.artists.items.forEach(function(item) {
+            if(item.name.toLowerCase() === artist.toLowerCase()) {
+                return callback(item);
+            }
+        })
+    }, function(err) {
+        console.log(err);
+    });
+};
+
+async function similarArtists(artist, callback) {
+    spotifyApi.getArtistRelatedArtists(artist)
+    .then(function(data){
+        return callback(data.body.artists);
+    }, function(err) {
+        console.log(err);
+    });
+}
 
 module.exports = {songRec};
