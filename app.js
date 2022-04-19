a7e7172376b66a9ffde3e29c78f3609751e9025f
Reed Salus <reedsalus@gmail.com>
2022-04-19 00:51:37 -0400

    code cleanup & commenting

--- app.js
+++ app.js
@@ -2,29 +2,26 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-require("dotenv").config();
-// remove dotenv in app.js and instead import from spotify auth
+const routes = require("./routes");
 
 // object imports
-const routes = require("./routes");
 const authRoutes = require('./routes.js');
-const { spotifyApi } = require('./spotify_authorization.js');
-//const functions = require('./spotify_functions');
-
+const { spotifyApi, secrets } = require('./spotify_authorization.js');
 const app = express();
-const port = process.env.PORT || 3000;
 
+// configure app
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(cors());
-
+app.use(routes);
 app.use('/api', cors(), authRoutes);
-app.set("port", port);
+
+// set values
+app.set("port", secrets.port);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(routes);
-
-app.listen(app.get("port"), () =>{
-  console.log("Server started on port "+app.get("port"));
+// launch
+app.listen(app.get("port"), () => {
+  console.log("Server started on port " + app.get("port"));
 });
--- routes.js
+++ routes.js
@@ -1,20 +1,38 @@
+// dependencies
 const express = require("express");
 const router = express.Router();
 const { authorizeURL, spotifyApi, scopes, getToken, refreshToken } = require("./spotify_authorization");
 
 router.get("/", (req, res) => {
-    res.render("index", {url:authorizeURL});
+    res.render("index", { url: authorizeURL });
 });
 
 router.get("/home", (req, res) => {
     res.render("home");
 });
 
-router.get('/callback', async (req, res) => {
+router.get("/query", (req, res) => {
+    res.render("findsongs");
+});
+
+router.post("/recommend", (req, res) => {
+    console.log(req.body.music)
+});
+
+// get login authorization from api
+router.get('/login', (req, res) => {
+      res.redirect(spotifyApi.createAuthorizeURL(scopes));
+});
+
+// handle callback from api; authenticate
+router.get("/callback", async (req, res) => {
+    // get code from query
     const code = req.query.code || null;
 
+    // request a token
     const response = await getToken(code);
     
+    // handle response; on success, set tokens
     if(response.status === 200) {
         spotifyApi.setAccessToken(response.data.access_token);
         spotifyApi.setRefreshToken(response.data.refresh_token);
@@ -25,29 +43,29 @@ router.get('/callback', async (req, res) => {
     // res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
 });
 
-router.get('/refresh_token', async (req, res) => {
+// refresh token (POST api); not used yet
+router.get("/refresh_token", async (req, res) => {
+    // get code from query
     const { refresh_token } = req.query;
 
+    //request a token
     const response = await refreshToken(refresh_token);
     
+    // handle response; on success, set token
     if(response.status === 200)
         spotifyApi.setAccessToken(response.data.access_token);
     else 
         res.send(response);
 });
 
-router.get("/query", (req, res) => {
-    res.render("findsongs");
-});
-
+// event listener for button in query.js; search functionality
 router.post("/submit", async (req, res) => {
-    //const token = await refreshToken(spotifyApi.getRefreshToken());
- 
     // example query layout... need to modify variable names in findsongs.ejs
     if(req.body.artist != null) {
         spotifyApi.searchArtists(req.body.artist)
             .then(
                 data => {
+                    // future improvement: replace artist with generic
                     res.render("songs", { artist: req.body.artist, data: data.body.artists.items })
                 }, 
                 err => { console.error(err); }
@@ -59,6 +77,7 @@ router.post("/submit", async (req, res) => {
         spotifyApi.getCategories(req.body.genre)
             .then(
                 data => {
+                    // future improvement: replace artist with generic
                     res.render("songs", { artist: req.body.genre, data: data.body.categories.items });
                 }, 
                 err => { console.error(err); }
@@ -67,12 +86,5 @@ router.post("/submit", async (req, res) => {
     
 });
 
-router.post("/recommend", (req, res) => {
-  console.log(req.body.music)
-})
-
-router.get('/login', (req, res) => {
-    res.redirect(spotifyApi.createAuthorizeURL(scopes));
-});
-
+// exports
 module.exports = router;
